3fb0227f8652ce41ecdd366fa73044bff8155de8
it21150548 Liyanage <87602577+it21150548@users.noreply.github.com>
2021-07-25 11:21:44 +0000

    I modified the parameters to reference type parameters

--- exercise04.cpp
+++ exercise04.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 void print(int len, int wth);
-void input(int len, int wth);
+void input(int &len, int &wth);
 
 // Do not change the main() function
 int main() {
@@ -17,5 +17,11 @@ void print(int len, int wth) {
    cout << "Length : " << len 
         << ", Width  : " << wth << endl;
 }
-
+void input(int &len, int &wth)
+{
+  cout<< "Length: ";
+  cin>> len;
+  cout<<"Width :";
+  cin>> wth;
+}
 // Implement the Input Function here
