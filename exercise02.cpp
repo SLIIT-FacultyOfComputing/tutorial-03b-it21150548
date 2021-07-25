a1bbb8a0683ffb68e3997a4833a9c56f8343d8b9
it21150548 Liyanage <87602577+it21150548@users.noreply.github.com>
2021-07-25 11:14:11 +0000

    I modified the program using a structure and a function

--- exercise02.cpp
+++ exercise02.cpp
@@ -5,20 +5,20 @@ int volume(int height, int width, int length);
 
 int main() {
     int box1Height, box1Width, box1Length;
-    int box2Height, box2Width, box2Length;
-    int totalVolume, totalSurface;
+ int box2Height, box2Width, box2Length;
+ int totalVolume, totalSurface;
     
     cout << "Enter Box 1 Height : ";
     cin >> box1Height;
     cout << "Enter Box 1 Width : ";
-    cin >> box1Width;
+    cin >>  box1Width;
     cout << "Enter Box 1 Length : ";
     cin >> box1Length;
     
      cout << "Enter Box 2 Height : ";
-    cin >> box2Height;
+    cin >>box2Height ;
     cout << "Enter Box 2 Width : ";
-    cin >> box2Width;
+    cin >>box2Width ;
     cout << "Enter Box 2 Length : ";
     cin >> box2Length;
     
@@ -29,5 +29,8 @@ int main() {
     
     return 0;
 }
-
+int volume(int height, int width, int length)
+{
+  return height*width*length;
+}
 // Implement the Volume() function here
